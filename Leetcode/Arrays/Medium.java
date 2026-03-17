cec22f13f3b8b7efe5803193860987c1b69757a0
jaykantyadavswe <jaykantyadavcse@gmail.com>
2026-03-17 07:29:17 +0530

    next permutations

---
+++ Leetcode/Arrays/Medium.java
@@ -0,0 +1,347 @@
+package DSA.Leetcode.Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import DSA.Leetcode.Strings.easy;
+
+public class Medium {
+    // 75. Sort Colors
+    public static void sortColors(int[] nums) {
+        int zeroNum = 0, oneNum = 0, twoNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeroNum++;
+            } else if (nums[i] == 1) {
+                oneNum++;
+            } else {
+                twoNum++;
+            }
+
+        }
+        int i = 0;
+        while (zeroNum-- > 0) {
+            nums[i++] = 0;
+        }
+
+        while (oneNum-- > 0) {
+            nums[i++] = 1;
+        }
+
+        while (twoNum-- > 0) {
+            nums[i++] = 2;
+        }
+    }
+
+    // Optimal Approach With Datch National Flag Algo
+    public static void sortColors2(int nums[]) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private static void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // 15. 3Sum
+    // Brute Force - O(n^3)
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    List<Integer> list = new ArrayList<>();
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        if (!lists.contains(list)) {
+                            lists.add(list);
+                        }
+                    }
+                }
+            }
+        }
+
+        return lists;
+    }
+
+    // Better Approach - Using HashSet
+    public static List<List<Integer>> threeSum2(int nums[]) {
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            HashSet<Integer> set = new HashSet<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                int third = -(nums[i] + nums[j]);
+                if (set.contains(third)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], third);
+
+                    Collections.sort(triplet);
+
+                    if (!lists.contains(triplet)) {
+                        lists.add(triplet);
+                    }
+                }
+
+                set.add(nums[j]);
+            }
+        }
+
+        return lists;
+    }
+
+    // Optimal Solution - O(nLogn + n^2)
+    public static List<List<Integer>> threeSum3(int nums[]) {
+        // Sorting
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            // skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                    ans.add(triplet);
+
+                    // Skip Duplicates
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    // 18. 4Sum
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> lists = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
+                for (int k = j + 1; k < n - 1; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        int sum = nums[i] + nums[j] + nums[k] + nums[l];
+                        if (sum == target) {
+                            List<Integer> res = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                            lists.add(res);
+                        }
+                    }
+                }
+            }
+        }
+
+        return lists;
+    }
+
+    // Optimal Approach
+    public static List<List<Integer>> fourSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> lists = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum == target) {
+                        List<Integer> res = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                        lists.add(res);
+                        k++;
+                        l--;
+
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+            }
+        }
+
+        return lists;
+    }
+
+    // Optimal Approach - Three loops → O(n³), Hash lookup → O(1)
+    public static List<List<Integer>> fourSum3(int[] nums, int target) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        HashSet<List<Integer>> res = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                HashSet<Long> set = new HashSet<>();
+                for (int k = j + 1; k < n; k++) {
+                    long newTarget = (long) target;
+                    newTarget -= nums[i];
+                    newTarget -= nums[j];
+                    newTarget -= nums[k];
+
+                    if (set.contains(newTarget)) {
+                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k], (int) newTarget);
+                        Collections.sort(list);
+                        res.add(list);
+                    }
+
+                    set.add((long) nums[k]);
+
+                }
+            }
+        }
+
+        ans.addAll(res);
+        return ans;
+    }
+
+    // Using Binary Search - > Sorting + Two Pointers -> O(n^3), sc-> O(1)
+    public static List<List<Integer>> fourSum4(int nums[], int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
+                for (int k = j + 1; k < n - 1; k++) {
+                    int newTarget = target;
+                    newTarget -= nums[i];
+                    newTarget -= nums[j];
+                    newTarget -= nums[k];
+                    // Find newTarget in nums using binary search from k+1 to n
+                    int low = k + 1;
+                    int high = n - 1;
+                    while (low <= high) {
+                        int mid = low + (high - low) / 2;
+                        if (nums[mid] == newTarget) {
+                            List<Integer> res = new ArrayList<>();
+                            res.add(nums[i]);
+                            res.add(nums[j]);
+                            res.add(nums[k]);
+                            res.add(newTarget);
+                            set.add(res);
+                            break;
+                        } else if (nums[mid] < newTarget) {
+                            low = mid + 1;
+                        } else {
+                            high = mid - 1;
+                        }
+                    }
+                }
+            }
+        }
+        ans.addAll(set);
+        return ans;
+    }
+
+    // 31. Next Permutation -> TC : O(n) & SC: O(1)
+    public static void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int pivot =-1;
+        // find pivot
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+
+        if(pivot == -1){
+            reverseArr(nums, 0, n-1); //in place changes
+            return;
+        }
+
+        // 2nd step: next larger element
+        for(int i=n-1; i>pivot; i--){
+            if(nums[i] > nums[pivot]){
+                int temp = nums[pivot];
+                nums[pivot] = nums[i];
+                nums[i] = temp;
+                break;
+            }
+        }
+
+        //  3rd step : reverse (piv+1 to n-1)
+        reverseArr(nums, pivot+1, n-1);
+    }
+
+    public static void reverseArr(int nums[], int i, int j){
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        // int nums[] = { 1, 0, -1, 0, -2, 2 };
+        // int nums[] = { 2, 2, 2, 2, 2 };
+        // int nums[] = { 1000000000, 1000000000, 1000000000, 1000000000 };
+        // sortColors2(nums);
+        // printArr(nums);
+        // System.out.println(threeSum3(nums));
+        // System.out.println(fourSum3(nums, -294967296));
+        // System.out.println(fourSum4(nums, 0));
+
+        int nums[] = {1, 1, 5};
+        nextPermutation(nums);
+        printArr(nums);
+
+    }
+
+    public static void printArr(int nums[]) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        System.out.println();
+    }
+}
