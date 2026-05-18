f858348183bdd11131f20cc2142bff42e0e01393
jaykantyadavswe <jaykant.yadav.cse@gmail.com>
2026-05-19 00:29:59 +0530

    Allocate Minimum Pages

--- Leetcode/Arrays/BinarySearch.java
+++ Leetcode/Arrays/BinarySearch.java
@@ -105,11 +105,59 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    // Allocate Minimum Pages
+    public static int minPagesAllocated(int arr[], int n, int m){//O(logN * n)
+        if(m > n) {
+            return -1;
+        }
+        int sum = 0;
+        for(int num : arr){ //O(n)
+            sum += num;
+        }
+
+        int ans = -1;
+        int st = 0, end = sum; //range of possible ans
+        while (st <= end) { 
+            int mid = st + (end - st) / 2;
+
+            if(isValid(arr, n, m, mid)){ //left
+                ans = mid;
+                end = mid - 1;
+            }else{ //right
+                st = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // O(n)
+    private static boolean isValid(int arr[], int n, int m, int maxAllowedPages){ //mid -> max Allowed Pages
+        int stu = 1, pages = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] > maxAllowedPages){
+                return false;
+            }
+
+            if(pages + arr[i] <= maxAllowedPages){
+                pages += arr[i];
+            }else{
+                stu++;
+                pages = arr[i];
+            }
+        }
+
+        return stu > m ? false : true;
+    }
     
     public static void main(String[] args) {
-        int arr[] = {1,1,2,3,3,4,4,8,8};
-        System.out.println(singleElement(arr));
+        int arr[] = {2, 1, 3, 4};
+        // System.out.println(singleElement(arr));
         /* int arr[] = {4,5,6,7,0,1,2};
         System.out.println(rotatedArr(arr, 0)); */
+
+        int n = 4, m = 2;
+        System.out.println(minPagesAllocated(arr, n, m));
     }
 }
