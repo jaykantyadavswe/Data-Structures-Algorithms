50fb7fa7c1a01a904baeecb87b464df01825b4b8
jaykantyadavswe <Jaykant.yadav.cse@gmail.com>
2026-09-18 08:55:53 +0530

    Continuous Subarray Sum

--- Leetcode/patterns/prefixSum.java
+++ Leetcode/patterns/prefixSum.java
@@ -172,6 +172,46 @@ public class prefixSum {
     }
     
 
+    // 523. Continuous Subarray Sum
+    // Brute force
+    public static boolean checkSubarraySum(int nums[], int k){
+        for(int i=0; i<nums.length; i++){
+            int sum = nums[i];
+            for(int j=i+1; j<nums.length; j++){
+                sum += nums[i];
+
+                if(sum % k == 0){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    // Optimal
+    public static boolean checkSubarraySum2(int nums[], int k){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int prefixSum = 0;
+
+        for(int i=0; i<nums.length; i++){
+            prefixSum += nums[i];
+
+            int rem = prefixSum % k;
+            System.out.println(rem);
+
+            if(map.containsKey(rem)){
+                if(i - map.get(rem) >= 2){
+                    return true;
+                }
+            }else{
+                map.put(rem, i);
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         /*
          * int nums[] = {1,2,3};
@@ -183,9 +223,13 @@ public class prefixSum {
         int k = 5;
         System.out.println(subarraysDivByK2(nums, k)); */
 
-        int nums[] = {1,2,3,4};
+        /* int nums[] = {1,2,3,4};
         int ans[] = productExceptSelf2(nums);
-        printArr(ans);
+        printArr(ans); */
+
+        int nums[] = {23,2,4,6,7};
+        int k = 6;
+        System.out.println(checkSubarraySum2(nums, k));
     }
 
     public static void printArr(int nums[]){
